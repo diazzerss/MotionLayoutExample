76c4210a22e6d48a61ade96124198f7516f0581b
Diaz <yuzikeevda@kamaz.ru>
2021-03-01 16:49:27 +0300

    commit 2

---
+++ app/src/main/java/com/example/myapplication/DummyRecyclerAdapter.kt
@@ -0,0 +1,25 @@
+package com.example.myapplication
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.recyclerview.widget.RecyclerView
+
+class DummyRecyclerAdapter : RecyclerView.Adapter<DummyRecyclerAdapter.ViewHolder>() {
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): DummyRecyclerAdapter.ViewHolder {
+        return ViewHolder(
+            LayoutInflater.from(parent.context)
+            .inflate(R.layout.dummy_list_item, parent, false) as ConstraintLayout
+        )
+    }
+
+    override fun getItemCount(): Int {
+        return 100
+    }
+
+    override fun onBindViewHolder(holder: DummyRecyclerAdapter.ViewHolder, position: Int) = Unit
+
+    class ViewHolder(val layout: ConstraintLayout) : RecyclerView.ViewHolder(layout)
+
+}
--- app/src/main/java/com/example/myapplication/MainActivity.kt
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -2,11 +2,19 @@ package com.example.myapplication
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val adapter = DummyRecyclerAdapter()
+
+        val rv: RecyclerView = findViewById(R.id.rv)
+        rv.adapter = adapter
+        rv.layoutManager = LinearLayoutManager(this)
 
 
     }
